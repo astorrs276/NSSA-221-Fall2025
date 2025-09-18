c13146683751846b0bc9af177d20122a3755565d
Alex Storrs <astorrs27@gmail.com>
2025-09-17 22:55:04 -0400

    commented and adjusted to fit rubric

--- ping_test.py
+++ ping_test.py
@@ -2,6 +2,10 @@
 import os
 import subprocess
 
+# Alexander Storrs
+# 9/17/2025
+
+# Function to print all of the command options
 def print_options():
     print('''
 1. Display the default gateway
@@ -10,12 +14,15 @@ def print_options():
 4. Test DNS resolution
 5. Exit/quit the script''')
 
+# Check for the default gateway dynamically
+# Uses `ip route` to get the gateway with `grep` and `awk` to parse it from the other info
 def default_gateway():
     command = "ip route | grep '^default' | awk '{print $3}'"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     gateway = result.stdout.strip()
     return gateway
 
+# Checks the local connection by pinging localhost
 def local_connection():
     command = ["ping", "-c", "2", "127.0.0.1"]
     try:
@@ -24,14 +31,16 @@ def local_connection():
     except subprocess.CalledProcessError as e:
         return "FAILED"
 
+# Checks the remote connection by pinging RIT's DNS server
 def remote_connection():
-    command = ["ping", "-c", "2", "8.8.8.8"]
+    command = ["ping", "-c", "2", "129.21.3.17"]
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         return result.stdout
     except subprocess.CalledProcessError as e:
         return "FAILED"
 
+# Checks the DNS connection by pinging Google
 def dns_connection():
     command = ["ping", "-c", "2", "www.google.com"]
     try:
@@ -40,20 +49,29 @@ def dns_connection():
     except subprocess.CalledProcessError as e:
         return "FAILED"
 
+# Main function to get the user's action and run everything accordingly
 def main():
     while True:
         print_options()
         choice = input(">> ")
         if choice == "1":
+            os.system("clear")
+            print("You selected option 1.")
             gateway = default_gateway()
             print("Default gateway:", gateway)
         elif choice == "2":
+            os.system("clear")
+            print("You selected option 2.")
             print("Testing Local connection:")
             print(local_connection())
         elif choice == "3":
+            os.system("clear")
+            print("You selected option 3.")
             print("Testing remote connection:")
             print(remote_connection())
         elif choice == "4":
+            os.system("clear")
+            print("You selected option 4.")
             print("Testing DNS:")
             print(dns_connection())
         elif choice == "5":
